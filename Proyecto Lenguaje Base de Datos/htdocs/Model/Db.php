3643d3c6f1484283a918df4ba7fa3f1296f8f07d
Jose Ricardo Quesada Rodríguez <71617494+Ricardo2011-Quesada@users.noreply.github.com>
2024-07-20 19:22:41 -0600

    Interfaz Grafica
    Creacion de la propuesta para la base de datos.
    Se crean las vistas mediante el uso de plantillas adaptandolas al proyecto

---
+++ Proyecto Lenguaje Base de Datos/htdocs/Model/Db.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+    private static $connection = null;
+
+    public static function getConnection() {
+        if (self::$connection == null) {
+            self::$connection = new mysqli('localhost:3306', 'root', '', 'casoestudio');
+            if (self::$connection->connect_error) {
+                die("Conexión fallida: " . self::$connection->connect_error);
+            }
+        }
+        return self::$connection;
+    }
+
+    public static function executeQuery($query, $params = []) {
+        $stmt = self::getConnection()->prepare($query);
+        if ($params) {
+            $stmt->bind_param(...$params);
+        }
+        $stmt->execute();
+        return $stmt;
+    }
+}
+?>
+
---
+++ Proyecto Lenguaje Base de Datos/htdocs/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/Login':
+        require __DIR__ . '/Controller/LoginController.php';
+        break;
+    case '/Libros':
+        require __DIR__ . '/Controller/LibrosController.php';
+        break;
+    case '/Categorias':
+        require __DIR__ . '/Controller/CategoriasController.php';
+        break;
+    case '/Editoriales':
+        require __DIR__ . '/Controller/EditorialesController.php';
+        break;
+        case '/Carrito':
+            require __DIR__ . '/Controller/CarritoController.php';
+            break;
+    default:
+        require __DIR__ . '/Controller/HomeController.php';
+        break;
+}
